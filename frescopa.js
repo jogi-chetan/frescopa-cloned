9f5af62ddfb023afb76332980501bbf321d769e0
Mark Szulc <mszulc@adobe.com>
2023-01-31 20:41:50 +1100

    updated JS

--- frescopa.js
+++ frescopa.js
@@ -1,5 +1,29 @@
 function intoCards(main) {
-    console.log("Frescopa Loaded Cards");
+    console.log("Frescopa Loaded More Cards");
+    main.querySelectorAll(".cards > div")
+    .forEach(column => {
+   
+      // first level is made up of columns
+      column.className = "column"
+   
+      let card = document.createElement("div");
+   
+      // we need to create a card div
+      card.className = "card";
+   
+      // move the picture
+      column.querySelectorAll("div > p > picture ").forEach(inner => {
+        card.appendChild(inner);
+      });
+   
+      // move the text
+      column.querySelectorAll("div > p  ").forEach(inner => {
+        card.appendChild(inner);
+      });
+   
+      column.appendChild(card)
+   
+    });
 }
 
 intoCards(document.querySelector("main"));
