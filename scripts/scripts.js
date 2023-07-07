1f0d30c2f6a86e3d232b225de01c9c203428f204
Mark Szulc <mszulc@adobe.com>
2023-07-07 23:02:17 +1000

    initial styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -52,7 +52,6 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
-  buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
 }
